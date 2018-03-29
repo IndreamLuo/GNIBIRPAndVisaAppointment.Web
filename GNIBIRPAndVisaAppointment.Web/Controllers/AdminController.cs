f786fd44f5d81cff796ca803819a1576fba2401d
Indream <indreamluo@qq.com>
2018-03-30 00:59:05 +0100

    Build Admin

---
+++ GNIBIRPAndVisaAppointment.Web/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GNIBIRPAndVisaAppointment.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string id, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
