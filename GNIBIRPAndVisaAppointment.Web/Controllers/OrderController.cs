f1b48eb09ab06d899f9834a6fce0eb98a2daa3b8
Indream Luo <indreamluo@qq.com>
2018-08-18 23:34:44 +0100

    Add order controller; put google ad.

---
+++ GNIBIRPAndVisaAppointment.Web/Controllers/OrderController.cs
@@ -0,0 +1,11 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GNIBIRPAndVisaAppointment.Web.Controllers
+{
+    [Route("Order")]
+    public class OrderController : Controller
+    {
+
+    }
+}
