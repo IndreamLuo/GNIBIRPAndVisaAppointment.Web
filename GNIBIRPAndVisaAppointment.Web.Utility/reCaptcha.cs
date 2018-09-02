15849b35087db182c4da96d71fe48b7cbbc15450
Indream Luo <indreamluo@qq.com>
2018-09-03 00:30:37 +0100

    Prepare Stripe apple and Google reCaptcha

---
+++ GNIBIRPAndVisaAppointment.Web.Utility/reCaptcha.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace GNIBIRPAndVisaAppointment.Web.Utility
+{
+    public class reCaptcha
+    {
+        public static bool Verify(string g_recaptcha_response)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
