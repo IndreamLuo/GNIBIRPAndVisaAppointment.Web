b52e833b052a4d78a3c711072a0be5270e5fc179
Indream Luo <indreamluo@qq.com>
2019-04-06 22:20:46 +0100

    Upgrade Fb sdk.

--- GNIBIRPAndVisaAppointment.Web/wwwroot/lib/facebook.sdk.js
+++ GNIBIRPAndVisaAppointment.Web/wwwroot/lib/facebook.sdk.js
@@ -3,7 +3,7 @@ window.fbAsyncInit = function() {
         appId: '1471497129644152',
         autoLogAppEvents: true,
         xfbml: true,
-        version: 'v2.12'
+        version: 'v3.2'
     });
 
     FB.AppEvents.logPageView();
@@ -17,10 +17,10 @@ window.fbAsyncInit = function() {
     });
 };
 
-(function(d, s, id){
+(function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
+    if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
